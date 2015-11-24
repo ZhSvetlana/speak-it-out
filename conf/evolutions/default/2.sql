5d13326a7d701a90b97c42d8984617cb00454e92
ZhSvetlana <svet_grodno@mail.ru>
2015-11-24 01:17:33 +0100

    Inserting data in database

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -10,18 +10,31 @@ INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (2, 
 INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (3, 'chicken', 'images/animals/chicken.png', 1, 1);
 INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (4, 'cow', 'images/animals/cow.png', 1, 1);
 INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (5, 'dog', 'images/animals/dog.png', 1, 1);
-INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (4, 'duck', 'images/animals/duck.png', 1, 1);
-
-INSERT INTO `USER`(`personID`,`email`,`firstname`,`lastname`,`birthday`,`address`,`phonenumber`,`pesel`,`password`,`typeOfAccount`, `numberOfPoints`) VALUES (100,'customer@gmail.com','Michalina','Gepart',NULL,NULL,NULL,NULL,'customer','CUSTOMER', 0);
-INSERT INTO `USER`(`personID`,`email`,`firstname`,`lastname`,`birthday`,`address`,`phonenumber`,`pesel`,`password`,`typeOfAccount`, `numberOfPoints`) VALUES (101,'owner@gmail.com','Marcelina','Izworska',NULL,NULL,NULL,NULL,'owner','OWNER', 0);
-INSERT INTO `USER`(`personID`,`email`,`firstname`,`lastname`,`birthday`,`address`,`phonenumber`,`pesel`,`password`,`typeOfAccount`, `numberOfPoints`) VALUES (102,'admin@gmail.com','Sveta','Zhuravitskaya',NULL,NULL,NULL,NULL,'admin','ADMIN', 0);
-
-
-INSERT INTO `Auto`(`autoID`,`color`,`year`,`brand`,`model`,`numberOfDoors`,`maxSpeed`,`airConditioning`,`numberOfPlaces`,`engineCapacity`,`price`,`points`,`localization`,`photoLink`) VALUES (1 ,'yellow',2014, 'Lamborghini','Huracan',2,400,'y',2,5,300,30,2,'carPictures/lamborghini_huracan.jpg');
-INSERT INTO `Auto`(`autoID`,`color`,`year`,`brand`,`model`,`numberOfDoors`,`maxSpeed`,`airConditioning`,`numberOfPlaces`,`engineCapacity`,`price`,`points`,`localization`,`photoLink`) VALUES (2 ,'white',2014, 'Fiat','Fiat500',2,200,'y',4,2,60,6,2,'carPictures/fiat_fiat500.jpg');
-INSERT INTO `Auto`(`autoID`,`color`,`year`,`brand`,`model`,`numberOfDoors`,`maxSpeed`,`airConditioning`,`numberOfPlaces`,`engineCapacity`,`price`,`points`,`localization`,`photoLink`) VALUES (3 ,'white',2014, 'Aston Martin','Vantage',2,200,'y',4,2,200,6,2,'carPictures/astonMartin_vantage.jpg');
-INSERT INTO `Auto`(`autoID`,`color`,`year`,`brand`,`model`,`numberOfDoors`,`maxSpeed`,`airConditioning`,`numberOfPlaces`,`engineCapacity`,`price`,`points`,`localization`,`photoLink`) VALUES (4 ,'orange',2014, 'Buddy','Electric',2,200,'y',4,2,20,6,2,'carPictures/buddy_electric.jpg');
-
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (6, 'duck', 'images/animals/duck.png', 1, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (7, 'fish', 'images/animals/fish.png', 1, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (8, 'mouse', 'images/animals/mouse.png', 1, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (9, 'pig', 'images/animals/pig.png', 1, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (10, 'rabbit', 'images/animals/rabbit.png', 1, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (11, 'camel', 'images/animals/camel.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (12, 'crocodile', 'images/animals/crocodile.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (13, 'elephant', 'images/animals/elephant.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (14, 'fox', 'images/animals/fox.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (15, 'frog', 'images/animals/frog.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (16, 'horse', 'images/animals/horse.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (17, 'lion', 'images/animals/lion.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (18, 'tiger', 'images/animals/tiger.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (19, 'wolf', 'images/animals/wolf.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (20, 'zebra', 'images/animals/zebra.png', 2, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (21, 'butterfly', 'images/animals/butterfly.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (22, 'crow', 'images/animals/crow.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (23, 'dolphin', 'images/animals/dolphin.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (24, 'eagle', 'images/animals/eagle.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (25, 'kangaroo', 'images/animals/kangaroo.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (26, 'parrot', 'images/animals/parrot.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (27, 'penguin', 'images/animals/penguin.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (28, 'pigeon', 'images/animals/pigeon.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (29, 'sheep', 'images/animals/sheep.png', 3, 1);
+INSERT INTO `WORD` (`id`,`spelling`,`picture`,`level`,`category_id`) VALUES (30, 'snake', 'images/animals/snake.png', 3, 1);
 
 # --- !Downs
 
